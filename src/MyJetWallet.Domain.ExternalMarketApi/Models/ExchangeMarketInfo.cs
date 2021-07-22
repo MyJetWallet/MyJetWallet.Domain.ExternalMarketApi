a8883721bab4f7a1b21ec803ca085333e82b83fb
geomatika46 <alexey.p@smplt.net>
2021-07-22 12:19:01 +0300

    add new fields to ExchangeMarketInfo

--- src/MyJetWallet.Domain.ExternalMarketApi/Models/ExchangeMarketInfo.cs
+++ src/MyJetWallet.Domain.ExternalMarketApi/Models/ExchangeMarketInfo.cs
@@ -11,5 +11,8 @@ namespace MyJetWallet.Domain.ExternalMarketApi.Models
         [DataMember(Order = 4)] public string BaseAsset { get; set; }
         [DataMember(Order = 5)] public string QuoteAsset { get; set; }
         [DataMember(Order = 6)] public int VolumeAccuracy { get; set; }
+        [DataMember(Order = 7)] public string AssociateInstrument { get; set; }
+        [DataMember(Order = 8)] public string AssociateBaseAsset { get; set; }
+        [DataMember(Order = 9)] public string AssociateQuoteAsset { get; set; }
     }
 }
