16f198e970a199ae009f1204fcedc578af394c6e
Maxim Gorin <mxmgorin@gmail.com>
2022-03-29 15:28:58 +0300

    Fix contract

--- src/MyJetWallet.Domain.ExternalMarketApi/Dto/GetTradesResponse.cs
+++ src/MyJetWallet.Domain.ExternalMarketApi/Dto/GetTradesResponse.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 using MyJetWallet.Domain.ExternalMarketApi.Models;
 
 namespace MyJetWallet.Domain.ExternalMarketApi.Dto;
 
+[DataContract]
 public class GetTradesResponse
 {
-    public bool IsError { get; set; }
-    public string ErrorMessage { get; set; }
-    public List<ExchangeTrade> Trades { get; set; }
+    [DataMember(Order = 1)] public bool IsError { get; set; }
+    [DataMember(Order = 2)] public string ErrorMessage { get; set; }
+    [DataMember(Order = 3)] public List<ExchangeTrade> Trades { get; set; }
 }
