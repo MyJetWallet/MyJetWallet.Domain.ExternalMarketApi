75aa38c1501e8f7183749548e1168f7ed470172f
Maxim Gorin <mxmgorin@gmail.com>
2022-03-29 17:19:51 +0300

    Add exchange name to get trades request

--- src/MyJetWallet.Domain.ExternalMarketApi/Dto/GetTradesRequest.cs
+++ src/MyJetWallet.Domain.ExternalMarketApi/Dto/GetTradesRequest.cs
@@ -8,4 +8,5 @@ public class GetTradesRequest
 {
     [DataMember(Order = 1)] public DateTime StartDate { get; set; }
     [DataMember(Order = 2)] public DateTime EndDate { get; set; }
+    [DataMember(Order = 3)] public string ExchangeName { get; set; }
 }
