7e1aded9846853f6ff5e25812418b0eb28004c0e
Maxim Gorin <mxmgorin@gmail.com>
2022-03-29 16:54:50 +0300

    Change get trades request

--- src/MyJetWallet.Domain.ExternalMarketApi/Dto/GetTradesRequest.cs
+++ src/MyJetWallet.Domain.ExternalMarketApi/Dto/GetTradesRequest.cs
@@ -6,7 +6,6 @@ namespace MyJetWallet.Domain.ExternalMarketApi.Dto;
 [DataContract]
 public class GetTradesRequest
 {
-    [DataMember(Order = 1)] public DateTime? StartDate { get; set; }
-    [DataMember(Order = 2)] public DateTime? EndDate { get; set; }
-    [DataMember(Order = 3)] public int Limit { get; set; }
+    [DataMember(Order = 1)] public DateTime StartDate { get; set; }
+    [DataMember(Order = 2)] public DateTime EndDate { get; set; }
 }
